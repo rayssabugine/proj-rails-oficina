9b513a34507cf8ee4c014ce520551456b4996f70
Rayssa Bugine <you@example.com>
2013-06-18 16:21:42 -0300

    Iniciando projeto

---
+++ proj-rails-oficina/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjRailsOficina::Application.config.secret_token = '593e16b4adbb3ec320402889e2ce085a1b3f702a851c2f2f2d4c226caa1dee602df8032996d77c0e14fead9fdd49deb201b0e58c923b9f2aa9739fca9f91ad6f'
